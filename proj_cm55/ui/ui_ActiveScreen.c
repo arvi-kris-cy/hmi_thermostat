5447cd94cca8f70cc2422e2c0d006dd7c1ba9fae
Jithesh <Jithesh.Raman@infineon.com>
2025-05-29 15:46:03 +0530

    Enabled LV_USE_DRAW_VG_LITE

--- proj_cm55/ui/ui_ActiveScreen.c
+++ proj_cm55/ui/ui_ActiveScreen.c
@@ -12,12 +12,12 @@ void ui_ActiveScreen_screen_init(void)
     lv_obj_remove_flag(ui_ActiveScreen, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
     
     #if defined (bg_color)
-    lv_obj_set_style_bg_color(ui_ActiveScreen, lv_color_hex(0x1A1B1A), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(ui_ActiveScreen, lv_color_hex(0x08665C), LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_bg_opa(ui_ActiveScreen, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-    // lv_obj_set_style_bg_grad_color(ui_ActiveScreen, lv_color_hex(0x18AB9B), LV_PART_MAIN | LV_STATE_DEFAULT);
-    // lv_obj_set_style_bg_main_stop(ui_ActiveScreen, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
-    // lv_obj_set_style_bg_grad_stop(ui_ActiveScreen, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-    // lv_obj_set_style_bg_grad_dir(ui_ActiveScreen, LV_GRAD_DIR_HOR, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_grad_color(ui_ActiveScreen, lv_color_hex(0x18AB9B), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_main_stop(ui_ActiveScreen, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_grad_stop(ui_ActiveScreen, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_grad_dir(ui_ActiveScreen, LV_GRAD_DIR_HOR, LV_PART_MAIN | LV_STATE_DEFAULT);
     #else 
     ui_backgroundimage = lv_image_create(ui_ActiveScreen);
     lv_image_set_src(ui_backgroundimage, &ui_img_background_png);
